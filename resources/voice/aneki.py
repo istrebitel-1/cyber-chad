bfa054fb206041ff3ffcffdac7f872af986b6465
istrebitel-1 <istrebitel.3.12@gmail.com>
2022-07-14 16:42:15 +0300

    deploy

--- resources/voice/aneki.py
+++ resources/voice/aneki.py
@@ -5,9 +5,6 @@ import bs4
 from gtts import gTTS
 
 
-synthesizer = pyttsx3.init()
-
-
 def get_anek():
     responce = requests.get('http://rzhunemogu.ru/RandJSON.aspx?CType=1')
     soup = bs4.BeautifulSoup(responce.text, 'html.parser')
